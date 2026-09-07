5adb052c0eb5ac913b3a1bc19237ffee7d86e15b
Digital24Online <piyarulislam.raj007@gmail.com>
2026-09-08 03:58:29 +0600

    Refactor Customer.fromMap to include custId and staffId

--- lib/main.dart
+++ lib/main.dart
@@ -511,9 +511,10 @@ class Customer {
 
   double get due => (bill - paid).clamp(0, double.infinity).toDouble();
 
-  factory Customer.fromMap(Map<String, dynamic> m) {
+    factory Customer.fromMap(Map<String, dynamic> m) {
     return Customer(
       id: (m['id'] as num?)?.toInt(),
+      custId: '${m['cust_id'] ?? ''}',
       userId: '${m['user_id'] ?? ''}',
       name: '${m['name'] ?? ''}',
       mobile: '${m['mobile'] ?? ''}',
@@ -523,10 +524,10 @@ class Customer {
       bill: ((m['total_bill'] ?? 0) as num).toDouble(),
       paid: ((m['total_paid'] ?? 0) as num).toDouble(),
       paymentDate: '${m['payment_date'] ?? ''}',
+      staffId: (m['staff_id'] as num?)?.toInt(),
       active: (m['status'] ?? 1) == 1,
     );
-  }
-}
+    }
 
 class BillingHomePage extends StatefulWidget {
   final bool english;
