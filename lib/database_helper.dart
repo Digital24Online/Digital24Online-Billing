ea797af362045f262dadb3a57ccc7a95571122c7
Digital24Online <piyarulislam.raj007@gmail.com>
2026-09-11 22:57:08 +0600

    Improve formatting of billings table creation SQL
    Refactor SQL table creation for better readability.

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -40,17 +40,18 @@ class DatabaseHelper {
 
   Future<void> _create(Database db, int version) async {
     await db.execute('''
-  CREATE TABLE billings (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE,
-    active INTEGER NOT NULL DEFAULT 1,
-    cloud_id TEXT NOT NULL DEFAULT '',
-    created_at TEXT NOT NULL,
-    updated_at TEXT NOT NULL
-  )
-''');
+      await db.execute('''
+      CREATE TABLE billings (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL UNIQUE,
+        active INTEGER NOT NULL DEFAULT 1,
+        cloud_id TEXT NOT NULL DEFAULT '',
+        created_at TEXT NOT NULL,
+        updated_at TEXT NOT NULL
+      )
+    ''');
 
-        await db.execute('''
+    await db.execute('''
       CREATE TABLE customers (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         billing_id INTEGER NOT NULL DEFAULT 1,
