aca1ca0c9bdf309919d70d5d4c817d5a654dd4e8
Digital24Online <piyarulislam.raj007@gmail.com>
2026-09-12 20:04:42 +0600

    Update database_helper.dart

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -291,6 +291,28 @@ class DatabaseHelper {
       'ON bills(customer_id)',
     );
 
+        Future<void> _seedDefaultBillings(Database db) async {
+    final now = DateTime.now().toIso8601String();
+
+    for (final name in [
+      'Billing 1',
+      'Billing 2',
+    ]) {
+      await db.insert(
+        'billings',
+        {
+          'name': name,
+          'active': 1,
+          'cloud_id': '',
+          'created_at': now,
+          'updated_at': now,
+        },
+        conflictAlgorithm:
+            ConflictAlgorithm.ignore,
+      );
+    }
+        }
+      
     await _seedDefaultBillings(db);
     await _seedDefaultPackages(db);
   }
